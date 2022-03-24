88936c1476f2b0d93e10f64c4756e225d564e1eb
Luan Cardoso <luanleitecardoso@gmail.com>
2022-03-24 17:49:33 -0300

    Deu certo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import QRCode from "qrcode.react";
 import * as api from "./api";
 import { useEffect, useState } from "react";
 import ImageCarousel from "../src/components/ImageCarousel";
+import { Button } from "@mui/material";
 // import images from "../src/components/Images";
 
 export default function App() {
@@ -33,7 +34,10 @@ export default function App() {
       {images.length > 0 ? 
 			(<><ImageCarousel images={images} /><div id="QRCodeWrapper">
           <QRCode id="QRCode" value="https://572e-201-49-245-32.ngrok.io/api/SafeBox/HackSafeBox/1" />
+          <p>QR Code para abrir o Cofre</p>
         </div></>) : (<p>Sem imgs</p>)}
+        <Button href="https://572e-201-49-245-32.ngrok.io/api/SafeBox/HackSafeBox/2">Roubar de Volta</Button>
+        <Button onClick={getImgs}>Atualizar</Button>
 		</div>
 	);
 }
