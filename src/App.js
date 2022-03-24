71dcd1f8d7de0bcaf33b96432d4edfddc1ec286c
Luan Cardoso <luanleitecardoso@gmail.com>
2022-03-24 14:03:54 -0300

    Criando QR Code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import QRCode from 'qrcode.react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <QRCode value="shorturl.at/btBO3" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
